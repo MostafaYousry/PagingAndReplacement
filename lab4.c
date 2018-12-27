ebe50d20bc6e2577b0177e61b40600b2676e6a07
Mostafa Yousry <mostafayousry8@gmail.com>
2018-12-27 13:59:58 +0000

    comment the code

--- lab4.c
+++ lab4.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-FILE *inputFile;
 int framesCount;
 int *framesArray;
 int *goblinArray;
@@ -12,6 +11,7 @@ char policyName[5];
 int *referencedPages;
 int referencedPagesCount = 0;
 
+/// check if the frames array contain a page and return its index if exists, otherwise return -1
 int isFramesArrayContainsPage(int pageNumber)
 {
     for(int i = 0; i < framesCount; i++)
@@ -22,6 +22,7 @@ int isFramesArrayContainsPage(int pageNumber)
     return -1;
 }
 
+/// print the content of the frames array
 void printFramesContent()
 {
     for(int i = 0; i < framesCount; i++)
@@ -31,7 +32,11 @@ void printFramesContent()
     }
     printf("\n");
 }
-
+/// FIFO replacement policy works as follow:
+/// . there is a pointer on the page to be replaced
+/// . if a page fault happened, this page will be replaced and the pointer will be on the next page in the array
+/// . repeat the previous steps in a circular way
+/// . count the number of pages faults after all the frames array is filled
 void startFIFO()
 {
     int index = 0;
@@ -61,10 +66,13 @@ void startFIFO()
     }
 }
 
+/// update the goblin array at specific index by the reference number
 void updateGoblinArray(int index, int referenceNumber)
 {
     goblinArray[index] = referenceNumber;
 }
+
+/// get the index of the minimum element in the goblin array so this is the index of the page will be replaced
 int LRUPageIndex()
 {
     int min = 10000000, minIndex;
@@ -78,8 +86,14 @@ int LRUPageIndex()
     }
     return minIndex;
 }
+
+/// LRU replacement policy work as follow:
+/// . replace the page that has not been referenced for the longest time
+/// . count the number of pages faults after all the frames array is filled
 void startLRU()
 {
+    goblinArray = (int*) calloc(framesCount,sizeof(int));
+    initializeByNegativeOne(goblinArray);
     int pageNumber, nextAvilableFrameIndex = 0, referenceNumber = 0;
     for(int i = 0; i < referencedPagesCount; i++)
     {
@@ -112,8 +126,17 @@ void startLRU()
     }
 }
 
+/// CLOCK replacement policy work as follow:
+/// . require the association of an additional bit with each frame (use bit)
+/// . when a page is firist loaded, the use bit is set to 1
+/// . frames array will be treated as circular array
+/// . the page that will be replaced, is the first page whose use bit = 0
+/// . when searching for that page if the use bit of a page = 1, set it to 0
+/// . if a page is existed in the frame array, set its use bit by 1
+/// . count the number of pages faults after all the frames array is filled
 void startCLOCK()
 {
+    useArray = (int*) calloc(framesCount,sizeof(int));
     int pageNumber, useArrayIndex = 0;
     for(int i = 0; i < referencedPagesCount; i++)
     {
@@ -148,24 +171,24 @@ void startCLOCK()
     }
 }
 
-void initializeByNegativeOne()
+/// set all the element of the array by -1
+void initializeByNegativeOne(int *array)
 {
     for(int i = 0; i < framesCount; i++)
     {
-        framesArray[i] = -1;
-        goblinArray[i] = -1;
+        array[i] = -1;
     }
 }
 
 void startPolicy()
 {
-
+    /// scan the number of frames and the policy name
     scanf("%d",&framesCount);
     scanf("%s", policyName);
+    /// allocate a space for frames array based on the frame then set all its element by -1
     framesArray = (int*) calloc(framesCount,sizeof(int));
-    goblinArray = (int*) calloc(framesCount,sizeof(int));
-    useArray = (int*) calloc(framesCount,sizeof(int));
-    initializeByNegativeOne();
+    initializeByNegativeOne(framesArray);
+    /// read the whole input until the user enters -1
     referencedPages = (int*) calloc(1,sizeof(int));
     int *temp;
     scanf("%d",&referencedPages[referencedPagesCount]);
@@ -178,6 +201,7 @@ void startPolicy()
         referencedPagesCount++;
     }
     referencedPagesCount--;
+    /// print policy name & some of the output lines format then start the replacement policy specified by the user
     printf("Replacement Policy = %s\n",policyName);
     printf("-------------------------------------\n");
     printf("Page   Content of Frames\n");
@@ -190,6 +214,8 @@ void startPolicy()
     else
         startCLOCK();
 
+
+    ///print the number of page faults happened
     printf("-------------------------------------\n");
     printf("Number of page faults = %d\n",pageFaultCount);
 
